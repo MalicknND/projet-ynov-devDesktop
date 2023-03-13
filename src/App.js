183519d5f4c9a99e737fc01fbe1b9ba4866306be
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-13 11:36:13 +0100

    save

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import fetchWeather from './api/fetchWeather';
 import './App.css';
 import Footer from './components/Footer';
+import styled from 'styled-components';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -18,39 +19,94 @@ function App() {
 
   return (
     <div className="main-container">
-      <input
+      <Search
         type="text"
-        className="search"
         placeholder="Enter your TimeZone Place"
         value={query}
         /** Direct State ge Query Send Madadhu*/ onChange={(e) =>
           setQuery(e.target.value)
         }
         onKeyPress={search}
-      ></input>
+      ></Search>
       {weather.main && (
-        <div className="city">
+        <City className="city">
           <h2 className="city-name">
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
-          <div className="city-temp">
+          <CityTemp className="city-temp">
             {Math.round(weather.main.temp)}
             <sup>&deg;C</sup>
-          </div>
-          <div className="info">
+          </CityTemp>
+          <CityInfo className="info">
             <img
               className="city-icon"
               src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
               alt={weather.weather[0].desccription}
             ></img>
             <p>{weather.weather[0].description}</p>
-          </div>
-        </div>
+          </CityInfo>
+        </City>
       )}
       <Footer />
     </div>
   );
 }
+const Search = styled.input`
+  outline: none;
+  padding: 15px 6.8%;
+  border-radius: 20px;
+  border: none;
+  margin-bottom: 3%;
+  background: rgba(250, 250, 250, 0.85);
+`;
+
+const City = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 40px 8%;
+  border-radius: 20px;
+  background: rgba(218, 213, 213, 0.85);
+  box-shadow: 10px 10px 5px 0px rgba(15, 15, 15, 0.404);
+
+  h2 {
+    font-size: 2em;
+  }
+  h2 sup {
+    padding: 0.2em 0.6em;
+    margin-left: 0.2em;
+    border-radius: 30px;
+    color: #fff;
+    background: #ff8c00;
+  }
+`;
+const CityTemp = styled.div`
+  font-size: 4rem;
+  font-weight: bold;
+  margin-top: 10px;
+  color: #1e2432;
+  text-align: center;
+  sup {
+    font-size: 0.5em;
+  }
+`;
+
+const CityInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    margin-top: 10px;
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+  }
+  img {
+    margin-top: 10px;
+    width: 100px;
+    height: 100px;
+  }
+`;
 
 export default App;
